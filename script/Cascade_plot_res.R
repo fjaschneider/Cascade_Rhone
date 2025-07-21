aa5d7eb3633c976d559bea80c21764390d128478
Fabio Andres Schneider <andress251@campus.unicaen.fr>
2025-07-21 18:55:17 +0200

    update site

--- script/Cascade_plot_res.R
+++ script/Cascade_plot_res.R
@@ -22,6 +22,168 @@ directory <- 'k:/Labo/20_OSR/Rhone_Fabio_Schneider/Result_Cascade_21_67reaches/r
 pastas <- list.dirs(path = directory, recursive = FALSE)
 save_directory <- "img/res_cascade/res_E2_eE&H/"
 
+
+## Transport capacity
+# Loop by folders
+for (pasta in pastas) {
+  
+  # Tenta encontrar o shapefile na pasta (assumindo que tem só um ou padrão)
+  arquivo_shp <- list.files(pasta, pattern = "\\.gpkg$", full.names = TRUE)
+  
+  if (length(arquivo_shp) == 0) {
+    message(paste("Nenhum shapefile encontrado na pasta:", pasta))
+    next
+  }
+  
+  # Lê o shapefile
+  shp <- st_read(arquivo_shp[1])
+  
+  shp$sum_cls_trc <- (shp$Tr_cap_boulder+shp$Tr_cap_gravel+shp$Tr_cap_sand)/1000
+  
+  l_dams <- shp[!is.na(shp$Dam),]
+  l_trib <- shp[!is.na(shp$Trib),]
+  
+  # Nome do ano (assumindo que o nome da pasta tem o ano, adapte se necessário)
+  ano <- basename(pasta)
+  
+  
+  ### Volume transported ###
+  # ggarange
+  
+  # discharge
+  p <- ggplot(shp) +
+    geom_line(aes(FromN, Qmean, colour = 'blue4'), lwd = 0.5) +
+    geom_text(aes(max(FromN), max(Qmean), label = 'Q mean'), vjust = 1, hjust = 0.2, size = 2) +
+    geom_line(aes(FromN, Qmin, colour = 'blue4'), lwd = 0.3, linetype = 5) +
+    geom_text(aes(max(FromN), max(Qmin), label = 'Q min'), vjust = 1, hjust = 0.2, size = 2) +
+    geom_line(aes(FromN, Qmax, colour = 'blue4'), lwd = 0.3, linetype = 5) +
+    geom_text(aes(max(FromN), max(Qmax), label = 'Q max'), vjust = 1, hjust = 0.2, size = 2) +
+    geom_vline(data = l_dams, aes(xintercept = FromN, colour = 'black'), linetype = 5, lwd = 0.2) +
+    geom_text(aes(FromN, altitude, label = Dam), angle = 90, vjust = -0.2, hjust = 1, size = 2, y = Inf) +
+    geom_segment(data = l_trib, aes(x = FromN, xend = FromN, y = Qmean+(max(Qmax)*0.2), yend = Qmean, colour = 'blue'), arrow = arrow(length = unit(0.15, 'cm')), linewidth = 0.5) +
+    geom_text(data = l_trib, aes(FromN, Qmean+(max(Qmax)*0.2), label = Trib), angle = 90, vjust = 0, hjust = 0, size = 2, color = 'blue') +
+    scale_y_continuous(name = expression('Discharge (m'^3~'.s'^-1~')'), n.breaks = 10, expand = expansion(c(0,0.05))) +
+    scale_x_continuous(n.breaks = 10) +
+    scale_fill_manual(breaks = c('blue4', 'orangered', 'coral4', 'black', 'blue'),
+                      values = c('blue4', 'orangered', 'coral4', 'black', 'blue'),
+                      labels = c('Discharge', 'Sediment transported', 'Altitude', 'Dams', 'Tributaries'), name = '') +
+    scale_colour_manual(breaks = c('blue4', 'orangered', 'coral4', 'black', 'blue'),
+                        values = c('blue4', 'orangered', 'coral4', 'black', 'blue'),
+                        labels = c('Discharge', 'Sediment transported', 'Altitude', 'Dams', 'Tributaries'), name = '') +
+    labs(x = 'Reach ID') +
+    theme_article(base_family = "Times New Roman") +
+    theme(legend.key.height = unit(0.5, 'cm'), legend.position = 'none', legend.spacing = unit(0, 'cm')) +
+    theme(text = element_text(size = 8, colour = 'black'),
+          axis.text = element_text(size = 8, colour = 'black'),
+          axis.text.x = element_blank(), axis.title.x = element_blank()) 
+  
+  
+  # Trcap Sand
+  p_trc_sand <- ggplot(shp) +
+    geom_col(aes(FromN, Tr_cap_sand/1000, fill = 'red'), color = 1, lwd = 0.1) +
+    geom_vline(data = l_dams, aes(xintercept = FromN, colour = 'black'), linetype = 5, lwd = 0.2) +
+    geom_vline(data = l_dams, aes(xintercept = FromN, colour = 'black'), linetype = 5, lwd = 0.2) +
+    geom_segment(data = l_trib, aes(x = FromN, xend = FromN, y = ((Tr_cap_sand/1000)+(max(shp$Tr_cap_sand/1000)*0.2)), yend = (Tr_cap_sand/1000), colour = 'blue'), arrow = arrow(length = unit(0.15, 'cm')), linewidth = 0.5) +
+    geom_text(data = l_trib, aes(FromN, ((Tr_cap_sand/1000)+(max(shp$Tr_cap_sand/1000)*0.2)), label = Trib), angle = 90, vjust = 0, hjust = 0, size = 2, color = 'blue') +
+    scale_y_continuous(name = 'Transport capacity - sand (1000 m'^3~')', n.breaks = 10, expand = expansion(c(0,0.05))) +
+    scale_x_continuous(n.breaks = 10) +
+    scale_fill_manual(breaks = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                      values = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                      labels = c('Discharge', 'Transport capacity', 'Altitude', 'Dams', 'Tributaries'), name = '') +
+    scale_colour_manual(breaks = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                        values = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                        labels = c('Discharge', 'Transport capacity', 'Altitude', 'Dams', 'Tributaries'), name = '') +
+    labs(x = 'Reach ID') +
+    theme_article(base_family = "Times New Roman") +
+    geom_hline(yintercept = 0, lwd = 0.2) +
+    theme(legend.key.height = unit(0.5, 'cm'), legend.position = 'none', legend.spacing = unit(0, 'cm')) +
+    theme(text = element_text(size = 8, colour = 'black'),
+          axis.text = element_text(size = 8, colour = 'black'),
+          axis.text.x = element_blank(), axis.title.x = element_blank())
+  
+  
+  
+  # Trcap gravel
+  p_trc_gravel <- ggplot(shp) +
+    geom_col(aes(FromN, Tr_cap_gravel/1000, fill = 'red'), color = 1, lwd = 0.1) +
+    geom_vline(data = l_dams, aes(xintercept = FromN, colour = 'black'), linetype = 5, lwd = 0.2) +
+    geom_vline(data = l_dams, aes(xintercept = FromN, colour = 'black'), linetype = 5, lwd = 0.2) +
+    geom_segment(data = l_trib, aes(x = FromN, xend = FromN, y = ((Tr_cap_gravel/1000)+(max(shp$Tr_cap_gravel/1000)*0.2)), yend = (Tr_cap_gravel/1000), colour = 'blue'), arrow = arrow(length = unit(0.15, 'cm')), linewidth = 0.5) +
+    geom_text(data = l_trib, aes(FromN, ((Tr_cap_gravel/1000)+(max(shp$Tr_cap_gravel/1000)*0.2)), label = Trib), angle = 90, vjust = 0, hjust = 0, size = 2, color = 'blue') +
+    scale_y_continuous(name = 'Transport capacity - gravel (1000 m'^3~')', n.breaks = 10, expand = expansion(c(0,0.05))) +
+    scale_x_continuous(n.breaks = 10) +
+    scale_fill_manual(breaks = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                      values = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                      labels = c('Discharge', 'Transport capacity', 'Altitude', 'Dams', 'Tributaries'), name = '') +
+    scale_colour_manual(breaks = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                        values = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                        labels = c('Discharge', 'Transport capacity', 'Altitude', 'Dams', 'Tributaries'), name = '') +
+    labs(x = 'Reach ID') +
+    theme_article(base_family = "Times New Roman") +
+    geom_hline(yintercept = 0, lwd = 0.2) +
+    theme(legend.key.height = unit(0.5, 'cm'), legend.position = 'none', legend.spacing = unit(0, 'cm')) +
+    theme(text = element_text(size = 8, colour = 'black'),
+          axis.text = element_text(size = 8, colour = 'black'),
+          axis.text.x = element_blank(), axis.title.x = element_blank())
+  
+  
+  
+  # Trcap boulder
+  p_trc_boulder <- ggplot(shp) +
+    geom_col(aes(FromN, Tr_cap_boulder/1000, fill = 'red'), color = 1, lwd = 0.1) +
+    geom_vline(data = l_dams, aes(xintercept = FromN, colour = 'black'), linetype = 5, lwd = 0.2) +
+    geom_vline(data = l_dams, aes(xintercept = FromN, colour = 'black'), linetype = 5, lwd = 0.2) +
+    geom_segment(data = l_trib, aes(x = FromN, xend = FromN, y = ((Tr_cap_boulder/1000)+(max(shp$Tr_cap_boulder/1000)*0.2)), yend = (Tr_cap_boulder/1000), colour = 'blue'), arrow = arrow(length = unit(0.15, 'cm')), linewidth = 0.5) +
+    geom_text(data = l_trib, aes(FromN, ((Tr_cap_boulder/1000)+(max(shp$Tr_cap_boulder/1000)*0.2)), label = Trib), angle = 90, vjust = 0, hjust = 0, size = 2, color = 'blue') +
+    scale_y_continuous(name = 'Transport capacity - boulder (1000 m'^3~')', n.breaks = 10, expand = expansion(c(0,0.05))) +
+    scale_x_continuous(n.breaks = 10) +
+    scale_fill_manual(breaks = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                      values = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                      labels = c('Discharge', 'Transport capacity', 'Altitude', 'Dams', 'Tributaries'), name = '') +
+    scale_colour_manual(breaks = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                        values = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                        labels = c('Discharge', 'Transport capacity', 'Altitude', 'Dams', 'Tributaries'), name = '') +
+    labs(x = 'Reach ID') +
+    theme_article(base_family = "Times New Roman") +
+    geom_hline(yintercept = 0, lwd = 0.2) +
+    theme(legend.key.height = unit(0.5, 'cm'), legend.position = 'none', legend.spacing = unit(0, 'cm')) +
+    theme(text = element_text(size = 8, colour = 'black'),
+          axis.text = element_text(size = 8, colour = 'black'),
+          axis.text.x = element_blank(), axis.title.x = element_blank())
+  
+  
+  
+  
+  # Trcap total
+  p_trc_total <- ggplot(shp) +
+    geom_col(aes(FromN, sum_cls_trc, fill = 'red'), color = 1, lwd = 0.1) +
+    geom_vline(data = l_dams, aes(xintercept = FromN, colour = 'black'), linetype = 5, lwd = 0.2) +
+    geom_vline(data = l_dams, aes(xintercept = FromN, colour = 'black'), linetype = 5, lwd = 0.2) +
+    geom_segment(data = l_trib, aes(x = FromN, xend = FromN, y = ((sum_cls_trc)+(max(shp$sum_cls_trc)*0.2)), yend = (sum_cls_trc), colour = 'blue'), arrow = arrow(length = unit(0.15, 'cm')), linewidth = 0.5) +
+    geom_text(data = l_trib, aes(FromN, ((sum_cls_trc)+(max(shp$sum_cls_trc)*0.2)), label = Trib), angle = 90, vjust = 0, hjust = 0, size = 2, color = 'blue') +
+    scale_y_continuous(name = 'Transport capacity - total (1000 m'^3~')', n.breaks = 10, expand = expansion(c(0,0.05))) +
+    scale_x_continuous(n.breaks = 10) +
+    scale_fill_manual(breaks = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                      values = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                      labels = c('Discharge', 'Transport capacity', 'Altitude', 'Dams', 'Tributaries'), name = '') +
+    scale_colour_manual(breaks = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                        values = c('blue4', 'red', 'coral4', 'black', 'blue'),
+                        labels = c('Discharge', 'Transport capacity', 'Altitude', 'Dams', 'Tributaries'), name = '') +
+    labs(x = 'Reach ID') +
+    theme_article(base_family = "Times New Roman") +
+    geom_hline(yintercept = 0, lwd = 0.2) +
+    theme(legend.key.height = unit(0.5, 'cm'), legend.position = 'bottom', legend.spacing = unit(0, 'cm')) +
+    theme(text = element_text(size = 8, colour = 'black'),
+          axis.text = element_text(size = 8, colour = 'black')) 
+  
+  
+  ptrc <- ggarrange(plots = list(p, p_trc_sand, p_trc_gravel, p_trc_boulder, p_trc_total), nrow = 5, ncol = 1)
+  
+  ggsave(filename = paste0(save_directory, 'plots_tr_cap-silt/tr_cap_', ano, '.png'),
+         plot = pvout, width = 16, height = 20, units = 'cm', dpi = 300)
+}
+
+
 ## Transported
 # Loop by folders
 for (pasta in pastas) {
